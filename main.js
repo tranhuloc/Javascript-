725547607829b3ab169c4e17d6384cd697ac7f15
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-03-08 12:22:46 +0700

    #9 Toan tu so hoc

--- main.js
+++ main.js
@@ -1,31 +1,18 @@
 /*
-Giới thiệu về toán tử trong JS
-1. Toán tử số học - Arithmetic
-2. Toán tử gán - Assignment
-3. Toán tử so sánh - Comparison
-4. Toán tử logic - Logical
-*/
+Toán tử số học
++       --> Cộng
+-       --> Trừ
+*       --> Nhân 
+**      --> Lũy thừa
+/       --> Chia
+%       --> Chia lấy dư  
+++      --> Tăng 1 giá trị số
+--      --> Giảm 1 giá trị số
+*/      
 
-//1. Toán tử số học - Arithmetic
-var a = 1 + 2;
+var a = 1;
+
+a--;
+a--;
+
 console.log(a);
-
-//2. Toán tử gán - Assignment => =
-var fullName = "Tran Huu Loc";
-console.log(fullName);
-
-//3. Toán tử so sánh - Comparison => (>, <, >=, <=, ==)
-var a = 1;
-var b = 2;
-
-if (a == b) {
-    alert("Dung");
-}
-
-//4. Toán tử logic - Logical
-var a = 1;
-var b = 2;
-
-if (a > 0 && b > 0) {
-    alert('a & b lon hon 0')
-}
