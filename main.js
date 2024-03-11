536a65b2fb8d19070433240a40bf91f20df25b6f
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-03-11 14:40:11 +0700

    Toan tu chuoi

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 /*
-Toán tử ++ --
-Prefix & Postfix
-*/      
+Toán tử chuỗi - String operator
+*/
 
-var a = 6;
- 
-var output = a++;
+// var firstName = 'Loc';
+// var lastName = 'Tran';
 
-//Việc 1: `a copy`, `a copy` = 6
-//Việc 2: +1 cho a, a = a + 1 => a = 7
-//Việc 3: trả về `a copy`
-console.log('output: ', output); //output = 6
+// console.log(firstName + ' ' + lastName);
 
+var name = "Loc";
 
-console.log('a: ', a); //a = 7
+name += " Tran";
+
+console.log(name);
