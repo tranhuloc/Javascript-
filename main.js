88a3d43418aec731ffcec40bf2d140d031b40610
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-03-14 13:37:34 +0700

    #33-f8 Data type

--- main.js
+++ main.js
@@ -1,31 +1,69 @@
 /*
-Toán tử logic
-1. && - And
-2. || - Or
-3. ! - Not
+1. Dữ liệu nguyên thủy - Primitive Data
+    - Number
+    - String
+    - Boolean
+    - Undefined
+    - Null
+    - Symbol
+
+2. Dữ liệu phức tạp - Complex Data
+    - Function
+    - Object
 */
 
+// Number type
 var a = 1;
 var b = 2;
-var c = 3;
+var c = 1.5;
 
-// &&
-// if (a > 0 && b > 0 && c > 0) {
-//     console.log("DIEU KIEN DUNG!");
-// } else {
-//     console.log("DIEU KIEN SAI!");
-// }
+// console.log(typeof a);
 
-// ||
-// if (a > 0 || b == 0 || c == 0) {
-//     console.log("DIEU KIEN DUNG!");
-// } else {
-//     console.log("DIEU KIEN SAI!");
-// }
+// String type
+var fullName = "Loc 'Tran";
 
-// !
-if (!(a < 0)) {
-    console.log("DIEU KIEN DUNG!");
-} else {
-    console.log("DIEU KIEN SAI!");
-}
+// console.log(typeof fullName);
+
+// Boolean type
+var isSuccess = true;
+
+// console.log(typeof isSuccess);
+
+// Undefined type
+var age;
+
+// console.log(typeof age);
+
+// Null type
+var isNull = null; //nothing
+
+// console.log(typeof isNull);
+
+// Symbol type
+var id = Symbol("id"); //unique
+var id2 = Symbol("id"); //unique
+
+// console.log(typeof id);
+
+// Function
+var myFunction = function () {
+    alert("Hi, Xin chao cac ban!");
+};
+
+// console.log(typeof myFunction);
+
+// myFunction();
+
+// Object types
+var myObject = {
+    name: "Loc Tran",
+    age: 18,
+    adress: "Can Tho",
+    myFunction: function () {},
+};
+
+// console.log(typeof myObject);
+
+var myArray = ["Javascript", "PHP", "C++", "Python", "C#"];
+
+// console.log(typeof myArray);
