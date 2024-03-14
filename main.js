0854cd9772a5963dfd47b4f234ebb065a200e215
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-03-14 13:44:37 +0700

    #35-f8 Toan tu so sanh P2

--- main.js
+++ main.js
@@ -1,69 +1,16 @@
 /*
-1. Dữ liệu nguyên thủy - Primitive Data
-    - Number
-    - String
-    - Boolean
-    - Undefined
-    - Null
-    - Symbol
-
-2. Dữ liệu phức tạp - Complex Data
-    - Function
-    - Object
+Toán tử so sánh - P2
 */
 
-// Number type
+/**
+ * ===
+ * !==
+ */
+
 var a = 1;
-var b = 2;
-var c = 1.5;
+var b = '1';
 
-// console.log(typeof a);
+console.log(a === b);
+// == chỉ so sánh value, === so sánh cả value và data type
 
-// String type
-var fullName = "Loc 'Tran";
-
-// console.log(typeof fullName);
-
-// Boolean type
-var isSuccess = true;
-
-// console.log(typeof isSuccess);
-
-// Undefined type
-var age;
-
-// console.log(typeof age);
-
-// Null type
-var isNull = null; //nothing
-
-// console.log(typeof isNull);
-
-// Symbol type
-var id = Symbol("id"); //unique
-var id2 = Symbol("id"); //unique
-
-// console.log(typeof id);
-
-// Function
-var myFunction = function () {
-    alert("Hi, Xin chao cac ban!");
-};
-
-// console.log(typeof myFunction);
-
-// myFunction();
-
-// Object types
-var myObject = {
-    name: "Loc Tran",
-    age: 18,
-    adress: "Can Tho",
-    myFunction: function () {},
-};
-
-// console.log(typeof myObject);
-
-var myArray = ["Javascript", "PHP", "C++", "Python", "C#"];
-
-// console.log(typeof myArray);
+console.log(a !== b);
