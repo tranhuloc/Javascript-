67d4ca43606208fdc2ee15b27a2b05781353ba80
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-03-19 21:52:36 +0700

    #51-f8 Hieu hon ve ham

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 /*
-    Return trong hàm
+    Một số điều cần biết về function
+
+    1. Khi function đặt trùng tên? -> Function định nghĩa sau sẽ ghi đè function định nghĩa trước
+    2. Khai báo biến trong hàm? -> Một biến được định nghĩa trong function thì phạm vi sử dụng chỉ nằm trong function đó
+    3. Định nghĩa hàm trong hàm -> Giống như biến, phạm vi sử dụng hàm trong hàm chỉ trong phạm vi hàm được định nghĩa
 */
 
-function sum(a, b) {
-    return a.toString() + b.toString(); //nối chuỗi
+//1. Khi function đặt trùng tên?
+// function showMessage() {
+//     console.log("Message 1");
+// }
+
+// function showMessage() {
+//     console.log("Message 2");
+// }
+
+// showMessage(); //-> Message 2
+
+
+//2. Khai báo biến trong hàm?
+// function showMessage() {
+//     var fullName = 'Loc Tran';
+
+//     console.log(fullName);
+// }
+
+// showMessage();
+
+//3. Định nghĩa hàm trong hàm
+function showMessage() {
+    function showMessage2() {
+        console.log('Message 2');
+    }
+
+    showMessage2();
 }
 
-var result = sum(2, 8);
-
-console.log(result);
+showMessage();
