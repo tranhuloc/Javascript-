ddf8e31881af64ca868d76900be2806cc488bf24
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-03-19 21:35:37 +0700

    #46-f8 Tu khoa return trong ham

--- main.js
+++ main.js
@@ -1,42 +1,11 @@
 /*
-Tham số hàm
-1. Tham số?
-    - Định nghĩa
-    - Kiểu dữ liệu
-    - Tính private
-    - 1 tham số
-    - Nhiều tham số
-
-2. Truyền tham số
-    - 1 tham số
-    - Nhiều tham số
-
-3. Arguments?
-    - Đối tượng arguments
-    - Giới thiệu vòng for
+    Return trong hàm
 */
 
-// function writeLog(message, message2) {
-//     console.log(message);
-//     console.log(message2);
-// }
-
-// writeLog('TEST', 'TEST 2');
-
-// Đối tượng arguments
-// function writeLog() {
-//     console.log(arguments);
-// }
-
-// writeLog('TEST 1', 'TEST 2');
-
-// Giới thiệu vòng for
-function writeLog() {
-    var myString = '';
-    for (var param of arguments) {
-        myString += `${param} - `
-    }
-    console.log(myString);
+function sum(a, b) {
+    return a.toString() + b.toString(); //nối chuỗi
 }
 
-writeLog("TEST 1", "TEST 2", "TEST 3");
+var result = sum(2, 8);
+
+console.log(result);
