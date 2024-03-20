da62055322070a2a1a93000335726b1bd5e6553c
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-03-20 16:12:22 +0700

    #54-f8 Kieu du lieu chuoi

--- main.js
+++ main.js
@@ -1,32 +1,50 @@
 /*
-    Các loại function
+    Chuỗi trong JS
 
-    1. Declaration function
-    2. Expression function
-    3. Arrow function
+    1. Tạo chuỗi
+        - Các cách tạo chuỗi
+        - Nên dùng cách nào? Lý do?
+        - Kiểm tra data type
+    2. Một số case sử dụng backslash (\)
+    3. Xem độ dài chuỗi
+    4. Chú ý độ dài khi viết code 
+    5. Template string ES6
 */
 
-//1. Declaration function -> Có thể được gọi trước khi được định nghĩa
-showMessage();
+// Các cách tạo chuỗi
+// Cách 1 -> Nên dùng cách 1, ngắn gọn, nhanh, 
+var fullName = "Loc Tran";
 
-function showMessage() {
-    console.log('Declaration function');
-}
+// Cách 2 -> Tạo ra kiểu dữ liệu không như mong muốn
+var fullName2 = new String("Loc Tran"); 
 
+console.log(typeof fullName2); // -> object != string
 
-//2. Expression function -> Không thể gọi trước khi được định nghĩa
-var showMessage2 = function testName() {
-    console.log('Expression function');
-}
+// 2. Một số case sử dụng backslash -> backlash in Javascript
+var fullName3 = 'Tran Huu \"Loc\'';
 
-showMessage2();
+console.log(fullName3);
 
-setTimeout(function testName() {
+var fullName4 = "Day la dau \\";
 
-});
+console.log(fullName4);
 
-var myObject = {
-    myFunction: function testName() {
+// 3. Xem độ dài chuỗi
+var fullName5 = "Loc Tran";
 
-    }
-}
+console.log(fullName5.length);
+
+// 4. Chú ý độ dài khi viết code -> tối đa 80 ký tự trên 1 dòng
+var fullName6 = "Một số case sử dụng backslash"
++ " 1. Một số case sử dụng backslash"
++ " 2. Một số case sử dụng backslash"
++ " 3. Một số case sử dụng backslash"
+;
+
+console.log(fullName6);
+
+// 5. Template string ES6
+var firstName = 'Loc';
+var lastName = 'Tran';
+
+console.log(`Toi la: ${firstName} ${lastName}`);
