827f9203c950768b382b0ffa015c6f70634f9a56
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-03-21 11:09:23 +0700

    #72-f8 Object

--- main.js
+++ main.js
@@ -1,81 +1,55 @@
 /*
-    Làm việc với Array
+    Object trong Javascript
 
-    Keyword: Javascript array methods
-
-    1. To string
-    2. Join
-    3. Pop
-    4. Push
-    5. Shift
-    6. Unshift
-    7. Splicing
-    8. Concat
-    9. Slicing
 */
 
-// 1. To string -> Chuyển kiểu array thành string
-var languages = ["Javascript", "PHP", "C++"];
+var sexKey = "sex";
 
-console.log(languages.toString()); // Javascript,PHP,C++
+var myInfo = {
+    name: "Loc Tran",
+    age: 18,
+    address: "Can Tho, VN",
+    [sexKey]: "male",
+};
 
-// 2. Join
-var languages2 = ["Javascript", "PHP", "C++"];
+console.log(myInfo); // {name: 'Loc Tran', age: 18, address: 'Can Tho, VN', sex: 'male'}
 
-console.log(languages2.join(" - ")); //Javascript - PHP - C++
+// Thêm key và value mới
+myInfo.email = "locth@gmail.com";
 
-// 3. Pop -> xóa phẩn tử ở cuối mảng và return phần tử đã xóa
-var languages3 = ["Javascript", "PHP", "C++"];
+console.log(myInfo); // {name: 'Loc Tran', age: 18, address: 'Can Tho, VN', sex: 'male', email: 'locth@gmail.com'}
 
-console.log(languages3.pop()); // C++
-console.log(languages3.pop()); // PHP
-console.log(languages3.pop()); // Javascript
-console.log(languages3.pop()); // undefined
+myInfo["my-phone"] = "0123456789";
 
-console.log(languages3); // []
+console.log(myInfo); // {name: 'Loc Tran', age: 18, address: 'Can Tho, VN', sex: 'male', email: 'locth@gmail.com', my-phone: '0123456789'}
 
-// 4. Push -> thêm 1 hoặc nhiều phần tử ở cuối mảng và return độ dài mới của mảng
-var languages4 = ["Javascript", "PHP", "C++"];
+// Lấy value ra
+console.log(myInfo.name); // Loc Tran
 
-console.log(languages4.push("Python", "Java")); // 5
+console.log(myInfo.class); // undefined
 
-console.log(languages4); // (4) ['Javascript', 'PHP', 'C++', 'Python', 'Java']
+console.log(myInfo["email"]); // locth@gmail.com
 
-// 5. Shift -> xóa phẩn tử ở đầu mảng và return phần tử đã xóa
-var languages5 = ["Javascript", "PHP", "C++"];
+var myKey = "address";
 
-console.log(languages5.shift()); // Javascript
-console.log(languages5.shift()); // PHP
-console.log(languages5.shift()); // C++
-console.log(languages5.shift()); // undefined
+console.log(myInfo[myKey]); // Can Tho, VN
 
-console.log(languages5); // []
+// Xóa 1 cặp key: value
+delete myInfo.sex;
 
-// 6. Unshift -> thêm 1 hoặc nhiều phần tử ở đầu mảng và return độ dài mới của mảng
-var languages6 = ["Javascript", "PHP", "C++"];
+console.log(myInfo); // {name: 'Loc Tran', age: 18, address: 'Can Tho, VN', email: 'locth@gmail.com', my-phone: '0123456789'}
 
-console.log(languages6.unshift("C#", "Python")); // 5
+var myInfo2 = {
+    name: "Loc Tran", // -> Thuộc tính / property
+    age: 18,
+    address: "Can Tho, VN", 
+    [sexKey]: "male",
+    getName: function() {  // -> Phương thức / method
+        return this.name; // myInfor2.name
+    },
+};
 
-console.log(languages6); // (5) ['C#', 'Python', 'Javascript', 'PHP', 'C++']
+// Function -> Phương thức / method
+// Others -> Thuộc tính / property
 
-// 7. Splicing -> Xóa, chèn phần tử vào vị trí bất kỳ trong mảng
-var languages7 = ["Javascript", "PHP", "C++"];
-
-languages7.splice(1, 2); // splice(vị trí bắt đầu xóa, số phần tử muốn xóa);
-languages7.splice(1, 0, "Python", "C#"); // splice(vị trí bắt đầu xóa, số phần tử muốn xóa, phần tử muốn chèn);
-
-console.log(languages7); // ['Javascript', 'Python', 'C#']
-
-// 8. Concat -> Nối array
-var languages8 = ["Java", "Python", "C#"];
-
-console.log(languages.concat(languages8)); // (6) ['Javascript', 'PHP', 'C++', 'Java', 'Python', 'C#']
-
-// 9. Slicing -> cắt vài phần tử or toàn bộ phần tử mảng
-var languages9 = ["Javascript", "PHP", "C++", "Python", "C#"];
-
-//slice(vị trí bắt đầu cắt, vị trí kết thúc cắt)
-
-console.log(languages9.slice(1, 4)); // (3) ['PHP', 'C++', 'Python']
-console.log(languages9.slice(-1)); // ['C#']
-console.log(languages9.slice(0, 1)); // ['Javascript']
+console.log(myInfo2.getName()); // Loc Tran
