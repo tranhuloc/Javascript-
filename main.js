7508afedea8cb611f2ff02af05f1657562e3b4f3
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-03-30 20:39:53 +0700

    #85-f8 Lenh re nhanh switch-case

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
 /*
-    Câu lệnh rẽ nhánh - If else
+    Câu lệnh rẽ nhánh - Switch
 */
 
-var date = 2;
+var date = 7;
 
-if (date === 2) {
-    console.log('Hom nay la thu 2');
-} else if (date === 3) {
-    console.log('Hom nay la thu 3');
-} else if (date === 4) {
-    console.log('Hom nay la thu 4');
-} else {
-    console.log('Khong phai ngay trong tuan');
+switch (date) {
+    case 2:
+        console.log("Hom nay la thu 2");
+        break;
+    case 3:
+        console.log("Hom nay la thu 3");
+        break;
+    case 4:
+        console.log("Hom nay la thu 4");
+        break;
+    case 5:
+        console.log("Hom nay la thu 5");
+        break;
+    case 6:
+        console.log("Hom nay la thu 6");
+        break;
+    case 7:
+        console.log("Hom nay la thu 7");
+        break;
+    default:
+        console.log("Khong biet");
 }
 
+// Khi so sánh tính đúng sai sử dụng if - else: > < !==
+// Khi cho trước giá trị và số case >= 3 sử dụng switch - case
 
-/*
-    Khi a chia hết cho 3 thì return về 1
-    Khi a chia hết cho 5 thì return về 2
-    Khi a chia hết cho 15 thì return về 3
-*/
-function run(a) {
-    if (a % 3 == 0 && a % 5 !== 0) {
-        return 1;
-    } else if (a % 5 == 0 && a % 15 !== 0) {
-        return 2;
-    } else if (a % 15 == 0) {
-        return 3;
+function run(fruits) {
+    var result;
+
+    switch (fruits) {
+        case "Banana":
+            result = "This is a Banana";
+            break;
+        case "Apple":
+            result = "This is an Apple";
+            break;
     }
+
+    return result;
 }
-
-
-// Kỳ vọng
-console.log(run(3)) // 1
-console.log(run(5)) // 2
-console.log(run(15)) // 3
