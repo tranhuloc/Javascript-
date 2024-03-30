168f633ce782827764e34950da6d3d9543087b58
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-03-30 21:17:19 +0700

    #90-f8 Gioi thieu vong lap

--- main.js
+++ main.js
@@ -1,29 +1,10 @@
 /*
-    Toán tử 3 ngôi - Ternary operator
+    Vòng lặp - Loop
+
+    1. for - Lặp với điều kiện đúng
+    2. for/in - Lặp qua key của đối tượng
+    3. for/of - Lặp qua value của đối tượng
+    4. while - Lặp khi điều kiện đúng
+    5. do/while - Lặp ít nhất 1 lần, 
+    sau đó lặp khi điều kiện đúng
 */
-
-var course = {
-    name: "Javascript",
-    coin: 0,
-};
-
-var result = course.coin > 0 ? `${course.coin} Coin` : "Mien phi";
-
-console.log(result);
-
-/*
-    Tạo hàm getCanVoteMessage, hàm này có 1 tham số là age. 
-    Trong trường hợp từ 18 tuổi trở lên 
-    hàm sẽ trả về Bạn có thể bỏ phiếu, 
-    ngược lại hàm trả về Bạn chưa được bỏ phiếu.
-*/
-
-function getCanVoteMessage(age) {
-    return age >= 18 ? 'Bạn có thể bỏ phiếu' : 'Bạn chưa được bỏ phiếu';
-}
-
-
-
-// Kỳ vọng
-console.log(getCanVoteMessage(18)) // 'Bạn có thể bỏ phiếu'
-console.log(getCanVoteMessage(15)) // 'Bạn chưa được bỏ phiếu'
