e85096a84e74ed5082d3baab308c805978f3953c
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-03-30 18:32:29 +0700

    #81-f8 Math object

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
 /*
-   Đối tượng Date
-
-   Search -> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+    Math object
+ 
+    - Math.PI
+    - Math.round()
+    - Math.abs()
+    - Math.ceil()
+    - Math.floor()
+    - Math.random()
+    - Math.min()
+    - Math.max()
 */
 
-var date = Date();
+console.log(Math.PI); // 3.141592653589793
+console.log(Math.round(1.5)); // 2
+console.log(Math.abs(-5.3));
+console.log(Math.ceil(4.00000000001)); // 5 -> LÀm tròn trên
+console.log(Math.floor(5.999999999)); // 5 -> LÀm tròn dưới 
+console.log(Math.random()); // Return 1 số thập phân ngẫu nhiên >1
+console.log(Math.floor(Math.random() * 10)); // Return 1 số ngẫu nhiên 1 - 10
+var random = Math.floor(Math.random() * 5);
 
-console.log(typeof date); //string
+var bonus = [
+    '10 coin',
+    '20 coin',
+    '30 coin',
+    '40 coin',
+    '50 coin',
+];
 
-var date = new Date();
+console.log(bonus[random]);
 
-console.log(date.getFullYear()); //2024
+console.log(Math.min(-110, 1, 0, 23, 99)); // -110
+console.log(Math.max(-110, 1, 0, 23, 99)); // 99
 
-var year = date.getFullYear();
-var month = date.getMonth() + 1; // 0 - 11 nếu không có +1
-var day = date.getDate();
-
-console.log(year); // 2024
-console.log(month); // 3
-console.log(day); // 30
-console.log(`${day}/${month}/${year}`); // 30/3/2024
-
-function getNextYear() {
-    return new Date().getFullYear() + 1;
-}
-
-console.log(getNextYear()); // 2025
+function getRandomItem(arr = []) {
+    return Math.random;  // hàm này nhận 1 tham số là mảng và sẽ trả về ngẫu nhiên 1 phần tử của mảng.
+} 
