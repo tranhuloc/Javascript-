9c36efbd3a443799502c1126fc2c8204fedaae27
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-04-02 20:37:50 +0700

    #94-f8 Bai tap Vong lap for

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 /*
-    For loop
+    Cho trước mảng numbers, 
+    hãy viết hàm getTotal 
+    trả về tổng giá trị các phần tử của mảng.
 */
 
-for(var i = 1; i <= 1000; i++) {
-    console.log('Xin chao ban den voi vong for');
+function getTotal(arr) {
+    var sum = 0;
+
+    for(var i = 0; i<arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
 }
+
+// Expected results
+// getTotal([1, 2, 3]) // Output: 6
+// getTotal([4, 5, -3]) // Output: 6
+// getTotal([4, 5, 3, 5]) // Output: 17
