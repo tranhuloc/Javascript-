52fc9df75c643b6f92046205f0fd4e774735c355
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-04-02 20:32:10 +0700

    #93-f8 Vong lap for - p2

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
 /*
-     Hãy tạo hàm getRandNumbers có 3 tham số là min, max, length. 
-     Hàm này sẽ trả về một mảng gồm length phần tử, 
-     các giá trị trong mảng là số ngẫu nhiên, 
-     giá trị trong khoảng từ min tới max.
-
-Gợi ý: 
-Math.random() * (max - min) + min 
-là cách tạo ra 1 số ngẫu nhiên trong khoảng min - max.
+    For loop
 */
 
-
-
-function getRandNumbers(min, max, length) {
-    var myArr = [];
-    for (var i = 1; i <= length; i++) {
-        myArr.push(Math.floor(Math.random() * (max - min) + min));
-    }
-    return myArr;
+for(var i = 1; i <= 1000; i++) {
+    console.log('Xin chao ban den voi vong for');
 }
