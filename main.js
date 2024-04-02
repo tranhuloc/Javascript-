d05a57c65d9a732cecfb183a93ee1917644e3d92
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-04-02 20:59:52 +0700

    #95-f8 Vong lap for - p3

--- main.js
+++ main.js
@@ -1,19 +1,57 @@
 /*
-    Cho trước mảng numbers, 
-    hãy viết hàm getTotal 
-    trả về tổng giá trị các phần tử của mảng.
+    For loop - p3
 */
 
-function getTotal(arr) {
-    var sum = 0;
+var myArray = [
+    'Javascript',
+    'PHP',
+    'C++',
+    'Python',
+    'C#',
+    'Ruby'
+];
 
-    for(var i = 0; i<arr.length; i++) {
-        sum = sum + arr[i];
+var arrayLength = myArray.length;
+
+for (var i = 0; i < arrayLength; i++) {
+    console.log(myArray[i]); // Javascript, PHP, C++, Python, C#, Ruby
+}
+
+
+/*
+Viết chương trình để tính tổng giá trị của 1 đơn hàng.
+
+Cho trước mảng orders là danh sách chứa các khóa học, 
+các mặt hàng này được thể hiện dưới dạng object 
+và đều có 1 key là price để thể hiện giá trị của mặt hàng đó.
+
+Bạn hãy hoàn thành hàm getTotal 
+để tính được tổng giá trị của đơn hàng.*/
+
+var orders = [
+    {
+        name: 'Khóa học HTML - CSS Pro',
+        price: 3000000
+    },
+    {
+        name: 'Khóa học Javascript Pro',
+        price: 2500000
+    },
+    {
+        name: 'Khóa học React Pro',
+        price: 3200000
+    }
+]
+
+function getTotal(orders) {
+    var sum = 0;
+    for (var i = 0; i < orders.length; i++) {
+        sum = sum + orders[i].price;
     }
     return sum;
 }
 
-// Expected results
-// getTotal([1, 2, 3]) // Output: 6
-// getTotal([4, 5, -3]) // Output: 6
-// getTotal([4, 5, 3, 5]) // Output: 17
+
+
+// Expected results:
+getTotal(orders) // Output: 8700000
