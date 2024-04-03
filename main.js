605aade8c7e0d3d54ce1e1a3db6067585c93527d
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-04-03 22:16:45 +0700

    #100-f8 Vong lap while

--- main.js
+++ main.js
@@ -1,46 +1,23 @@
 /*
-    For/of loop 
-    Sử dụng để lấy ra từng phần tử của 1 mảng
-    or lấy ra từng chữ cái của 1 chuỗi
-    không thể sử dụng với object - nếu sử dụng cần biến đổi thêm
+    While loop 
 */
 
-var languages = ["Javascript", "PHP", "C++"];
+var i = 0;
 
-for (var value of languages) {
-    console.log(value); // Javascript PHP C++
+while (i < 100) {
+    i++;
+    console.log(i); // 1 -> 100
 }
 
+var myArray = [
+    'Javascript',
+    'PHP',
+    'C++'
+];
 
+var a = 0;
 
-var languages1 = "Javascript";
-
-for (var value of languages1) {
-    console.log(value); // J a v a s c r i p t
-}
-
-
-
-var myInfo = {
-    name: "Loc Tran",
-    age: 18,
-};
-
-// Trả lại mảng các key từ object
-console.log(Object.keys(myInfo)); // (2) ['name', 'age']
-
-for (var value of Object.keys(myInfo)) {
-    console.log(value); // name age -> Lấy ra các key từ object
-}
-
-// Trả lại mảng các value từ object
-console.log(Object.values(myInfo)); // (2) ['Loc Tran', 18]
-
-// Lấy ra value từ object
-for (var value of Object.keys(myInfo)) {
-    console.log(myInfo[value]); // Loc Tran 18
-}
-
-for (var value of Object.values(myInfo)) {
-    console.log(value); // Loc Tran 18
+while (a < myArray.length) {
+    console.log(myArray[a]); // Javascript PHP C++
+    a++;
 }
