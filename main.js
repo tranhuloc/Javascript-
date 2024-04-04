3c8803d62af55b11ef1ccc6fe5f488bbeef015d0
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-04-04 09:53:51 +0700

    #101-f8 Vong lap Do...while

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 /*
-    While loop 
+    Do...While loop -> từ lần lặp thứ 2 mới kiểm tra điều kiện
 */
 
 var i = 0;
 
-while (i < 100) {
+do {
     i++;
-    console.log(i); // 1 -> 100
-}
 
-var myArray = [
-    'Javascript',
-    'PHP',
-    'C++'
-];
+    console.log(i); // 1 -> 10
 
+} while (i < 10);
+
+
+
+var isSuccess = false;
 var a = 0;
 
-while (a < myArray.length) {
-    console.log(myArray[a]); // Javascript PHP C++
+do {
     a++;
-}
+    console.log('Nạp thẻ lần ' + a);
+
+    // Thành công
+    if (true) {
+        isSuccess = true;
+    }
+
+} while (!isSuccess && a < 3)
