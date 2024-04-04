5be8671bb4c7e2cac175a89cd03148d80b579485
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-04-04 12:47:23 +0700

    #105-f8 De quy

--- main.js
+++ main.js
@@ -1,15 +1,67 @@
 /*
-    Ví dụ về vòng lặp
+    Đệ quy
 */
 
-for (var i = 50; i > 0; i--) {
-    console.log(i); // 50 -> 1
+// Xoá phần tử trùng lặp
+ var array = ['a', 'b', 'c', 'a', 'b', 'c'];
+
+ // Đặc tính của Set() là các elements or values chỉ tồn tại 1
+ console.log(new Set(array)); // Set(3) {'a', 'b', 'c'}
+
+ // ... -> toán tử giải
+ console.log([...(new Set(array))]); // (3) ['a', 'b', 'c']
+
+
+
+/*
+    1. Xác định điểm dừng
+    2. Logic handle -> Tạo ra điểm dừng
+*/
+
+ function countDown(num) {
+    if(num > 0) {
+
+        // Loop
+        console.log(num);
+        return countDown(num - 1);
+    }
+    return num;
+ }
+
+ countDown(5);
+
+ function loop(start, end, cb) {
+    if(start <= end) {
+        cb(start);
+        return loop(start + 1, end, cb);
+    }
+ }
+
+
+ var array = ['Javascript', 'PHP', 'C++'];
+
+ loop(0, array.length - 1, function(index) {
+    //console.log('index: ', index); // index:  0  index:  1  index:  2
+    console.log(array[index]); // Javascript PHP C++
+})
+
+// Tính giai thừa
+// Cách 1
+function giaiThua(number) {
+    var output = 1;
+        for (var i = number; i > 0; i--) {
+            output = output * i;
+        }
+        return output;
 }
 
-for (var j = 0; j <= 50; j += 5) {
-    console.log(j); // 0 5 10 15 ... 50
+//Cách 2
+function giaiThua2(number) {
+    if (number > 0) {
+        return number * giaiThua2(number - 1);
+    }
+    return 1;
 }
 
-for (var k = 50; k >= 0; k -= 5) {
-    console.log(k); // 50 45 40 35 ... 0
-}
+console.log(giaiThua(6)); //720
+console.log(giaiThua2(5)); //120
