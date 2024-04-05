aeeab265d3f275a9af2531e6b581f711e7ed9720
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-04-05 17:44:26 +0700

    #108-f8 Array map() method

--- main.js
+++ main.js
@@ -9,53 +9,56 @@
         reduce()
 */
 
-
-// filter() -> return danh sách các phần tử muốn tìm kiếm
-var listCourse = courses.filter(function (course, index) {
-    return course.name === "PHP";
-});
-
-console.log(listCourse); // (2) [{…}, {…}]  0: {id: 4, name: 'PHP', coin: 400}  1: {id: 6, name: 'PHP', coin: 700}
-
-/*
-Hãy tạo hàm getMostFavoriteSport 
-có 1 tham số 
-(F8 sẽ gọi hàm này và luôn truyền đối số là 1 array). 
-Hàm getMostFavoriteSport sẽ trả về các môn thể thao 
-có điểm số yêu thích lớn hơn 5.
-
-Gợi ý: Sử dụng phương thức filter.
-*/
-
-const sports = [
+var courses = [
     {
-        name: 'Bóng rổ',
-        like: 6
+        id: 1,
+        name: "Javascript",
+        coin: 100,
     },
     {
-        name: 'Bơi lội',
-        like: 5
+        id: 2,
+        name: "HTML, CSS",
+        coin: 0,
     },
     {
-        name: 'Bóng đá',
-        like: 10
+        id: 3,
+        name: "Ruby",
+        coin: 0,
     },
-]
+    {
+        id: 4,
+        name: "PHP",
+        coin: 15,
+    },
+    {
+        id: 5,
+        name: "ReactJS",
+        coin: 12,
+    },
+    {
+        id: 6,
+        name: "PHP",
+        coin: 50,
+    },
+];
 
-function getMostFavoriteSport(array) {
-    var listSport = array.filter(function(item, index) {
-        listSport = item.like > 5
-    });
-    return listSport;
+// map() -> dùng để chỉnh sửa các element của 1 array, 
+// return 1 mảng mới có số lượng phẩn tử = số lượng phần tử có giá trị của mảng cũ
+// function courseHandler(course) {
+//     console.log(course)
+// }
+
+// var newCourses = courses.map(courseHandler);
+
+function courseHandler(course, index) {
+    return {
+        id: course.id,
+        name: `Khoa hoc: ${course.name}`,
+        coin: course.coin,
+        coinText: `Gia: ${course.coin}`,
+        index: index,
+    };
 }
 
 
-
-// Kỳ vọng
-console.log(getMostFavoriteSport(sports)) 
-// Output: [{ name: 'Bóng rổ, like: 6 }, { name: 'Bóng đá, like: 10 }]
-
-
-
-
-
+var newCourses = courses.map(courseHandler);
