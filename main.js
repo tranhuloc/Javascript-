d40897848f80565fb8ef06692f4edd01ffbfa0ae
tranhuloc <tran.huu.loc.17082k2@gmail.com>
2024-04-07 21:19:36 +0700

    #109-f8 Array reduce() method

--- main.js
+++ main.js
@@ -42,23 +42,84 @@ var courses = [
     },
 ];
 
-// map() -> dùng để chỉnh sửa các element của 1 array, 
-// return 1 mảng mới có số lượng phẩn tử = số lượng phần tử có giá trị của mảng cũ
-// function courseHandler(course) {
-//     console.log(course)
-// }
+// reduce() -> sử dụng khi muốn nhận về 1 giá trị duy nhất 
+// sau khi tính toán trên những phần tử của 1 array
+var totalCoin = 0;
 
-// var newCourses = courses.map(courseHandler);
-
-function courseHandler(course, index) {
-    return {
-        id: course.id,
-        name: `Khoa hoc: ${course.name}`,
-        coin: course.coin,
-        coinText: `Gia: ${course.coin}`,
-        index: index,
-    };
+// Không dùng reduce()
+for (var course of courses) {
+    totalCoin += course.coin;
 }
 
+console.log(totalCoin);
 
-var newCourses = courses.map(courseHandler);
+
+// Dùng reduce()
+var i = 0;
+
+function coinHandler(accumulator, currentValue, currentIndex, originArray) {
+    i++;
+
+    var total = accumulator + currentValue.coin;
+
+    console.table({
+        'Lượt chạy: ': i,
+        'Biến tích trữ: ': accumulator,
+        'Giá khóa học: ': currentValue.coin,
+        'Tích trữ được: ': total
+    });
+    console.log(currentValue);
+
+    // return accumulator + currentValue.coin;
+    return total;
+}
+
+var totalCoin2 = courses.reduce(coinHandler, 0);
+
+console.log(totalCoin2);
+
+
+
+/*
+    Tại SEA GAMES 31 vừa qua, 
+    đoàn thể thao Việt Nam đã đứng đầu bảng tổng sắp huy chương. 
+    Hãy tạo hàm getTotalGold có 1 tham số là mảng. 
+    Tính tổng số huy chương vàng 
+    mà đoàn thể thao Việt Nam đạt được 
+    trong kỳ SEA Game lần này.
+*/
+
+
+var sports = [
+    {
+        name: 'Bơi lội',
+        gold: 11
+    },
+    {
+        name: 'Boxing',
+        gold: 3
+    },
+    {
+        name: 'Đạp xe',
+        gold: 4
+    },
+    {
+        name: 'Đấu kiếm',
+        gold: 5
+    },
+]
+
+function getTotalGold(sports) {
+    return sports.reduce(function(sum, arr) {
+        return sum + arr.gold;
+    }, 0)
+};
+
+// Expected results:
+console.log(getTotalGold(sports)) // Output: 23
+
+
+
+
+
+
